f2883adf511be50d9cc0e2eb9650cbed67127f45
zeroventuno <zeroventuno.cc@gmail.com>
2025-11-25 09:47:33 +0100

    Feature: Add copyright footer with Ventuno branding
    - Added centered footer with copyright text in Italian
    - Footer displays: 'Copyright © 2025 Ventuno. Tutti i diritti riservati.'
    - Styled with gray background and border
    - Uses flexbox layout to stay at bottom of page

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -4,13 +4,20 @@ import { Sidebar } from './Sidebar';
 
 export default function Layout() {
     return (
-        <div className="min-h-screen bg-background">
+        <div className="min-h-screen bg-background flex flex-col">
             <Sidebar />
-            <main className="pl-64">
+            <main className="pl-64 flex-1">
                 <div className="container mx-auto p-8">
                     <Outlet />
                 </div>
             </main>
+            <footer className="pl-64 py-4 bg-gray-100 border-t border-gray-200">
+                <div className="container mx-auto px-8">
+                    <p className="text-center text-sm text-gray-600">
+                        Copyright © 2025 Ventuno. Tutti i diritti riservati.
+                    </p>
+                </div>
+            </footer>
         </div>
     );
 }
